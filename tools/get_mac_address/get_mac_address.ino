a127455b773aeef2fea380541d64589ced69b095
aksh-72 <aaksah722@gmail.com>
2026-09-19 17:33:12 +0530

    Create get_mac_address.ino
    Adds a helper sketch to read and display the ESP-NOW peer pairing.

---
+++ tools/get_mac_address/get_mac_address.ino
@@ -0,0 +1,12 @@
+#include <WiFi.h>
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  
+  WiFi.mode(WIFI_MODE_STA);
+  delay(100);
+  Serial.println(WiFi.macAddress());
+}
+void loop() {
+}
